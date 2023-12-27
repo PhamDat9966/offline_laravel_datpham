03dda494df4f12d2f5980657091bf118cce45e7a
PhamDat <phamdat999666@gmail.com>
2023-12-28 04:09:06 +0700

    7.3.3.Đọc Rss Thanhnien

--- app/Helpers/Feed.php
+++ app/Helpers/Feed.php
@@ -3,15 +3,29 @@ namespace App\Helpers;
 
 class Feed{
     public static function read($itemRss){
+        // echo '<pre>';
+        // print_r($itemRss);
+        // echo '</pre>';
         $result = [];
+
         foreach ($itemRss as $value) {
-            $data   = Feed::readVNExpress($value['link']);
-            $result = array_merge_recursive($result,$data);
+            switch ($value['source']) {
+                case 'thanhnien':
+                    $data   = Feed::readThanhnien($value['link']);
+                    $result = array_merge_recursive($result,$data);
+                    break;
+                case 'vnexpress':
+                    $data   = Feed::readVNExpress($value['link']);
+                    $result = array_merge_recursive($result,$data);
+                    break;
+            }
+
         }
+
         return $result;
     }
 
-    public static function readVNExpress($link ){
+    public static function readVNExpress($link){
 
         try {
             $context = stream_context_create([
@@ -40,6 +54,44 @@ class Feed{
                 $data[$key]['description']  = $tmp2[1] ?? $value['description'];
                 $data[$key]['thumb']        = $tmp1[1] ?? '';
             }
+            return $data;
+        } catch (\Throwable $th) {
+            return [];
+        }
+
+    }
+
+    public static function readThanhnien($link){
+
+        try {
+            $context = stream_context_create([
+                'http' => [
+                    'protocol_version' => '1.1',
+                    'header' => 'Upgrade: HTTP/1.1'
+                ]
+            ]);
+
+            $xmlString = file_get_contents($link, false, $context);
+
+            $data = simplexml_load_string($xmlString, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $data = json_encode($data);
+            $data = json_decode($data, TRUE);
+            $data = $data['channel']['item'];
+
+            foreach ($data as $key => $value) {
+                unset($data[$key]['guid']);
+                $tmp1 = [];
+                $tmp2 = [];
+
+                $pattern = '/<img[^>]*\s*src=["\']([^"\']*)["\']/i';
+                preg_match($pattern,$value['description'], $tmp1);
+
+                $tmp2[1] = preg_replace('/<[^>]*>/', '', $value['description']);
+
+                $data[$key]['description']  = $tmp2[1];
+                $data[$key]['thumb']        = $tmp1[1] ?? '';
+            }
+
             return $data;
         } catch (\Throwable $th) {
             return [];
