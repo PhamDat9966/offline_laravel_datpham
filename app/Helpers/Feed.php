c53b3cd26e2f6d061f93df577a1b824d71604a19
PhamDat <phamdat999666@gmail.com>
2023-12-28 04:15:41 +0700

    7.3.3.Đọc Rss TuoiTre

--- app/Helpers/Feed.php
+++ app/Helpers/Feed.php
@@ -9,7 +9,7 @@ class Feed{
         $result = [];
 
         foreach ($itemRss as $value) {
-            switch ($value['source']) {
+            switch (Feed::checkSourceLink($value['source'], $value['link'])) {
                 case 'thanhnien':
                     $data   = Feed::readThanhnien($value['link']);
                     $result = array_merge_recursive($result,$data);
@@ -18,11 +18,20 @@ class Feed{
                     $data   = Feed::readVNExpress($value['link']);
                     $result = array_merge_recursive($result,$data);
                     break;
+                case 'tuoitre':
+                    $data = Feed::readTuoiTre($value['link']);
+                    break;
             }
 
         }
 
         return $result;
+    }
+
+    public static function checkSourceLink($source, $link)
+    {
+        $sourceFromLink = explode('.', parse_url($link, PHP_URL_HOST))[0];
+        return ($source == $sourceFromLink);
     }
 
     public static function readVNExpress($link){
@@ -59,6 +68,31 @@ class Feed{
             return [];
         }
 
+    }
+
+    public static function readTuoiTre($link) // name
+    {
+        try {
+            $data = simplexml_load_file($link, "SimpleXMLElement", LIBXML_NOCDATA);
+            $data = json_encode($data);
+            $data = json_decode($data, TRUE);
+            $data = $data['channel']['item'];
+
+            foreach ($data as $key => $value) {
+                unset($data[$key]['guid']);
+                $tmp1 = [];
+                $tmp2 = [];
+
+                preg_match('/src="([^"]*)"/i', $value['description'], $tmp1);
+                preg_match('/.*<\/a>(.*)/', $value['description'], $tmp2);
+
+                $data[$key]['description'] = $tmp2[1] ?? $value['description'];
+                $data[$key]['thumb'] = $tmp1[1] ?? '';
+            }
+            return $data;
+        } catch (\Throwable $th) {
+            return [];
+        }
     }
 
     public static function readThanhnien($link){
